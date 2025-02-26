fb84ffbc5de043d2d879f26bd594cee154c4e1bb
JoseTellez09 <josetellez224@unisangil.edu.co>
2025-02-26 15:03:28 -0500

    transformacines variables

---
+++ transfomaconesgit.py
@@ -0,0 +1,14 @@
+nombre = input("ingrese su nombre")
+print (nombre)
+print(type(nombre))
+#edad
+edad = int(input("ingrese su edad"))
+print(edad)
+print(type(edad))
+#estado de matricula
+estado = bool(input("estado de matricula"))
+print(estado)
+print(type(estado))
+#peso
+peso = float(input("ingrese su peso: "))
+print (type(peso))
